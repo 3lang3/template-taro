2d9d17c812addeef2876ff8f5812738149162017
项文俊 <xiangwenjun1@zhenhuipin.cn>
2021-07-05 18:05:52 +0800

    feat: 完成编辑信息地址修改

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,18 +1,21 @@
+import { useState } from 'react';
 import Flex from '@/components/Flex';
 import Typography from '@/components/Typography';
 import { Image, View } from '@tarojs/components';
 import { navigateTo } from '@tarojs/taro';
-import { AtList, AtListItem } from 'taro-ui';
-import { getMusicsongmakeList } from '@/services/profile';
+import { AtList, AtListItem, AtIcon } from 'taro-ui';
+import { getMusicsongmakeList, Node, editInfo } from '@/services/profile';
+import AreaPicker from '@/components/CustomPicker/AreaPicker';
 import { useRequest } from 'ahooks';
 import { FullPageLoader, FullPageError } from '@/components/Chore';
 import './index.less';
 
 export default () => {
-  const { loading, error, refresh, run } = useRequest(getMusicsongmakeList, {
-    onSuccess: (res) => {
-      if (res.type === 1) throw Error(res.msg);
-      console.log(res);
+  const [memberInfo, setMemberInfo] = useState<Node>({} as any);
+  const { loading, error, refresh } = useRequest(getMusicsongmakeList, {
+    onSuccess: ({ data, type, msg }) => {
+      if (type === 1) throw Error(msg);
+      setMemberInfo(data);
     },
   });
   if (loading) return <FullPageLoader />;
@@ -20,22 +23,27 @@ export default () => {
   return (
     <View className="page-profile">
       <Flex className="profile-header" justify="between">
-        <Image
-          className="profile-header__avatar"
-          src={require('@/assets/icon/avatar_default.svg')}
-        />
+        <Image className="profile-header__avatar" src={memberInfo.avatar} />
         <Typography.Title className="profile-header__name" type="light" level={1} ellipsis>
           Hi音乐人
         </Typography.Title>
       </Flex>
-      <AtList className="custom-list">
+      <AtList className="custom-list custom-form">
         <AtListItem title="张三" disabled />
         <AtListItem title="32323****44884" disabled />
         <AtListItem
           title="75****2@163.com"
           onClick={() => navigateTo({ url: '/pages/profile/email' })}
         />
-        <AtListItem title="浙江省 杭州市 西湖区" arrow="right" />
+        <View className="custom-form-picker">
+          <AreaPicker
+            value={[memberInfo.province, memberInfo.city, memberInfo.district]}
+            onChange={(value) => {
+              editInfo(value);
+            }}
+          />
+          <AtIcon className="item-extra__icon" value="chevron-right" />
+        </View>
         <AtListItem
           title="134****4332"
           onClick={() => navigateTo({ url: '/pages/profile/mobile' })}
--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,20 +1,35 @@
 import request, { PromiseResponseType } from '@/utils/request';
+import type { SuccessResType } from './common.d';
 
 export type Node = {
-  member_ids: Number;
-  avatar: String;
-  real_name: String;
-  idcard: String;
-  mobile: String;
-  province: String;
-  city: String;
-  district: String;
-  email: String;
+  member_ids: number;
+  avatar: string;
+  real_name: string;
+  idcard: string;
+  mobile: string;
+  province: string;
+  city: string;
+  district: string;
+  email: string;
 };
 
 // 歌曲制作列表
-export function getMusicsongmakeList(): Promise<PromiseResponseType<any>> {
+export function getMusicsongmakeList(): Promise<PromiseResponseType<Node>> {
   return request('/member/getEditInfo', {
     method: 'GET',
+  });
+}
+
+// 歌曲制作列表
+export function editInfo([province, city, district]: number[]): Promise<
+  PromiseResponseType<SuccessResType>
+> {
+  return request('/member/editInfo', {
+    method: 'POST',
+    data: {
+      province,
+      city,
+      district,
+    },
   });
 }
