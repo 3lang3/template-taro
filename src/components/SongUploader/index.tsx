e123660ec247daaa35ad69f07372d34e1a16f78f
3lang <675483520@qq.com>
2021-07-06 15:52:35 +0800

    fix: SongUploader status

--- src/components/SongUploader/index.tsx
+++ src/components/SongUploader/index.tsx
@@ -1,30 +1,47 @@
+import { useRef } from 'react';
 import cls from 'classnames';
-import { View, Text, Image } from '@tarojs/components';
+import { View, Text } from '@tarojs/components';
 import { chooseMessageFile, setClipboardData } from '@tarojs/taro';
 import { AtInput } from 'taro-ui';
 import Flex from '../Flex';
 import Typography from '../Typography';
 import './index.less';
+import Icon from '../Icon';
 
 type SongUploaderProps = {
+  value?: any;
   className?: string;
   webActionUrl?: string;
   name?: string;
-  onChange?: (v: any) => void;
+  onChange?: (v: string | undefined, file?: chooseMessageFile.ChooseFile, response?: any) => void;
+  chooseMessageFileType?: chooseMessageFile.Option['type'];
 };
 
 export default (props: SongUploaderProps) => {
+  const chooseFileRef = useRef<chooseMessageFile.ChooseFile>();
+
   const copy = async () => {
     if (!props.webActionUrl) return;
     setClipboardData({ data: props.webActionUrl });
   };
+
+  const onDelete = () => {
+    chooseFileRef.current = undefined;
+    if (props.onChange) props.onChange(undefined);
+  };
+
   const onUploadClick = async () => {
-    // @todo upload file pipe
-    const { errMsg, tempFiles } = await chooseMessageFile({ count: 1 });
+    const { errMsg, tempFiles } = await chooseMessageFile({
+      count: 1,
+      type: props.chooseMessageFileType || 'file',
+    });
     if (errMsg !== 'chooseMessageFile:ok') throw Error(errMsg);
     const [file] = tempFiles;
-    if (props.onChange) props.onChange(file);
+    chooseFileRef.current = file;
+    // @todo upload file pipe
+    if (props.onChange) props.onChange(file.path, file, {});
   };
+
   return (
     <View className={cls('settlein-list', props.className)}>
       <Flex className="settlein-list__item border">
@@ -52,18 +69,36 @@ export default (props: SongUploaderProps) => {
         </Flex>
       </View>
       <View className="p-default">
-        <View onClick={onUploadClick} className="border--bolder p-default">
-          <Flex justify="center">
-            <Typography.Text type="secondary">添加歌曲</Typography.Text>
-            <Image
-              className="settlein-uploader__icon"
-              src={require('@/assets/icon/play_thin_outline.svg')}
-            />
+        {props.value ? (
+          <Flex
+            direction="column"
+            justify="center"
+            className="settlein-uploader__status settlein-uploader__status--success"
+          >
+            <Icon onClick={onDelete} className="settlein-uploader__delete" icon="icon-ashbin" />
+            <Flex justify="center">
+              <Icon className="settlein-uploader__icon mr10" icon="icon-icon_qupu" />
+              <Typography.Title style={{ margin: 0 }} level={3} type="light">
+                歌曲添加成功!
+              </Typography.Title>
+            </Flex>
+            {chooseFileRef.current ? (
+              <Typography.Text center type="light" size="sm">
+                {chooseFileRef.current.name}
+              </Typography.Text>
+            ) : null}
           </Flex>
-          <Typography.Text center type="secondary" size="sm">
-            选择一个上传音频的聊天对话窗口
-          </Typography.Text>
-        </View>
+        ) : (
+          <View onClick={onUploadClick} className="border--bolder p-default">
+            <Flex justify="center">
+              <Typography.Text type="secondary">添加歌曲</Typography.Text>
+              <Icon className="settlein-uploader__icon" icon="icon-shouye_bofang" />
+            </Flex>
+            <Typography.Text center type="secondary" size="sm">
+              选择一个上传音频的聊天对话窗口
+            </Typography.Text>
+          </View>
+        )}
       </View>
     </View>
   );
