3b17e198e7ec8973e53344cc91a68cc5f648fe5b
3lang <675483520@qq.com>
2021-07-08 14:37:13 +0800

    docs: 新增注释

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -4,18 +4,23 @@ import CustomTabBar from '@/components/CustomTabBar';
 import MePage from './components/MePage';
 import CompanyPage from './components/CompanyPage';
 
-const PageContentWithTabbar = () => {
+const PageContent = () => {
   const { loading, data } = useSelector((state) => state.common);
 
   if (loading) return <FullPageLoader />;
+  // 不同身份视图不同
+  // 机构身份渲染机构视图
   if (data.identity === 3) return <CompanyPage />;
   return <MePage />;
 };
 
+/**
+ * @summary tab类页面不能阻塞底部导航条渲染, 页面内容请求和默认页面渲染分离
+ */
 export default () => {
   return (
     <>
-      <PageContentWithTabbar />
+      <PageContent />
       <CustomTabBar />
     </>
   );
