1aac001607844611427cb6a06e987a8c67b065f6
3lang <675483520@qq.com>
2021-07-19 10:43:46 +0800

    docs: app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -11,35 +11,20 @@ import './style/global.less';
 //     silentLogin();
 //     getCustomNavigationInfo();
 //   }, []);
-
-//   // 对应 onShow
-//   useDidShow(() => {
-//     const r = getCurrentInstance();
-//     console.log(r)
-//   });
-
 //   return <Provider store={store}>{children}</Provider>;
 // };
 
 class App extends Component<any, any> {
-  // 可以使用所有的 React 生命周期方法
   componentDidMount() {
     silentLogin();
     getCustomNavigationInfo();
   }
-
-  // 对应 onLaunch
+  // 获取场景值
   onLaunch(opts) {
-    console.log(opts);
+    console.log(opts.scence);
   }
   render() {
-    // 在入口组件不会渲染任何内容，但我们可以在这里做类似于状态管理的事情
-    return (
-      <Provider store={store}>
-        {/* this.props.children 是将要被渲染的页面 */}
-        {this.props.children}
-      </Provider>
-    );
+    return <Provider store={store}>{this.props.children}</Provider>;
   }
 }
 
