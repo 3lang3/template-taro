a10070b6713a8a325fda3b0cd9cd5738c33f65b2
3lang <675483520@qq.com>
2021-07-20 14:47:24 +0800

    chore: update docs

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,22 @@ import { Component } from 'react';
 import store from '@/state/config/store';
 import { silentLogin } from './utils/login';
 import { getCustomNavigationInfo } from './components/CustomNavigation/helper';
-// taro-ui 组件样式
+
+/**
+ * taro-ui 组件依赖的样式文件
+ * @summary 全局导入和按需导入打包后体积相差100+kb
+ */
 import 'taro-ui/dist/style/index.scss';
+// import 'taro-ui/dist/style/components/button.scss';
+// import 'taro-ui/dist/style/components/activity-indicator.scss';
+// import 'taro-ui/dist/style/components/loading.scss';
+// import 'taro-ui/dist/style/components/modal.scss';
+// import 'taro-ui/dist/style/components/list.scss';
+// import 'taro-ui/dist/style/components/icon.scss';
+// import 'taro-ui/dist/style/components/checkbox.scss';
+// import 'taro-ui/dist/style/components/form.scss';
+// import 'taro-ui/dist/style/components/input.scss';
+
 import './style/global.less';
 
 class App extends Component<any, any> {
