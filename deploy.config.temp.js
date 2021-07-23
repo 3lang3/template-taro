d8b5afce113cee2943336ef1f3a0075cbaf14adf
3lang <675483520@qq.com>
2021-07-23 14:12:26 +0800

    docs: updatee

--- deploy.config.temp.js
+++ deploy.config.temp.js
@@ -1,4 +1,11 @@
-// 部署配置
+/**
+ * 部署配置
+ *
+ * - 复制.env.temp文件重命名为.env, 更改其配置
+ * - 复制此文件重命名为deploy.config.js
+ * - 开发测试使用`npm run deploy:preview`打包
+ * - 生产环境使用`npm run deploy:upload`打包
+ */
 module.exports = {
   // 私钥文件路径
   privateKeyPath: '/Users/3lang/Desktop/private.key',
