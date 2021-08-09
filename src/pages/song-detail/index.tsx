344aa00b94ef4dd7c7d049933b326b4c3b34b22d
项文俊 <xiangwenjun1@zhenhuipin.cn>
2021-08-09 09:52:36 +0800

    fix: 1011389 【歌曲制作】制作详情均为空

--- src/pages/song-detail/index.tsx
+++ src/pages/song-detail/index.tsx
@@ -2,6 +2,7 @@ import cls from 'classnames';
 import Flex from '@/components/Flex';
 import Typography from '@/components/Typography';
 import { View } from '@tarojs/components';
+import { getCurrentInstance } from '@tarojs/taro';
 import { getMakeSongDetail, Node } from '@/services/song-detail';
 import { FullPageLoader, FullPageError, Empty } from '@/components/Chore';
 import { useRequest } from 'ahooks';
@@ -9,7 +10,8 @@ import { useState } from 'react';
 import './index.less';
 
 export default () => {
-  const ids = 527875806;
+  const { router } = getCurrentInstance();
+  const { ids } = (router as any).params;
   const [list, setList] = useState<Node[]>([]);
 
   const { loading, error, refresh } = useRequest(getMakeSongDetail, {
